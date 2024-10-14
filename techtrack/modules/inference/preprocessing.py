30db68be5c85ca819c53edf277bf4319c5132175
AI Whisperer <133732746+MechInterpreter@users.noreply.github.com>
2024-10-13 18:50:51 -0700

    Add files via upload

--- techtrack/modules/inference/preprocessing.py
+++ techtrack/modules/inference/preprocessing.py
@@ -1,32 +1,22 @@
 import cv2 as cv
 
-def capture_video(stream_url, drop_rate):
-    # Use OpenCV's VideoCapture to read from a UDP stream using FFMPEG
-    cap = cv.VideoCapture(stream_url, cv.CAP_FFMPEG)
+def capture_video(filename, drop_rate):
+    # Instantiate VideoCapture object
+    cap = cv.VideoCapture(filename)
     
-    # Check if the capture is successfully opened
-    if not cap.isOpened():
-        print("Error: Unable to open video stream.")
-        return
-    
+    # Establish frame count
     frame_count = 0
     
-    # Loop through video frames
-    while True:
+    # Ensure object is opened, read in frames, and increment frame count
+    while cap.isOpened():
         ret, frame = cap.read()
         
         if not ret:
-            print("Stream ended or cannot read frame.")
             break
         
-        # Process only every 'drop_rate' frame
         if frame_count % drop_rate == 0:
-            # Instead of showing the frame, save it to disk
-            frame_filename = f"output_frames/frame_{frame_count}.jpg"
-            cv.imwrite(frame_filename, frame)
-            print(f"Frame {frame_count} saved to {frame_filename}")
-        
+            yield frame
+            
         frame_count += 1
-    
-    # Release resources
+        
     cap.release()
